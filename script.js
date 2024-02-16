02d8e4ce381cd90c78ad4c99d5a394761051bf5b
Saimon Ahmed <67533318+SAIMON-AHMED@users.noreply.github.com>
2024-02-16 11:59:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@
 var compose = function(functions) {
     
     return function(x) {
-        let sum = 0;
         for (let i = functions.length - 1; i >= 0; i--) {
             x = functions[i](x);
         }
